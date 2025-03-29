7b0e388319c58d1b2e4a417109602d8d539cbe27
Aleydon <aleydonroberto@gmail.com>
2025-03-29 09:37:23 -0300

    test: Checks if get methods are defined in UserEntity and their return type

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -26,4 +26,27 @@ describe('UserEntity', () => {
 		);
 		expect(sut.props.createdAt).toBeInstanceOf(Date);
 	});
+
+	it('getter of name field', () => {
+		expect(sut.props.name).toBeDefined();
+		expect(sut.name).toBe(props.name);
+		expect(typeof sut.name).toBe('string');
+	});
+
+	it('getter of email field', () => {
+		expect(sut.props.email).toBeDefined();
+		expect(sut.email).toBe(props.email);
+		expect(typeof sut.email).toBe('string');
+	});
+
+	it('getter of password field', () => {
+		expect(sut.props.password).toBeDefined();
+		expect(sut.password).toBe(props.password);
+		expect(typeof sut.password).toBe('string');
+	});
+
+	it('getter of createdAt field', () => {
+		expect(sut.props.createdAt).toBeDefined();
+		expect(sut.createdAt).toBeInstanceOf(Date);
+	});
 });
